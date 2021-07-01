de146b44a0919076b058d2153706a79e1c0b40e8
Worrawut Poonsuk <worrawutp@gmail.com>
2021-07-01 21:03:51 +0700

    Extract pokemon pack from collection and add them to cards node

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
+import pokemonCollection from './pokemons.json'
+
 import './main.scss'
+
+document.addEventListener('DOMContentLoaded', function(){
+  const PACK = 20
+  const cards = document.getElementById('cards')
+  const pokemons = pokemonCollection.slice(0, PACK)
+
+  pokemons.forEach(pokemon => {
+    let cardDOM = buildCardDOM(pokemon)
+    cards.insertAdjacentHTML('beforeend', cardDOM) 
+  })
+})
+
+function buildCardDOM(pokemon) {
+  let typeString = ''
+  pokemon.type.forEach(t => {
+    typeString += `<div class="poke-type ${t.toLowerCase()}">${t}</div>`
+  })
+
+  return (
+    `<div class="card">
+      <a href="" class="card-photo">
+        <img src="${pokemon.ThumbnailImage}" alt="${pokemon.ThumbnailAltText}" />
+      </a>
+      <div class="poke-info">
+        <div class="poke-number">#${pokemon.number}</div>
+        <div class="poke-name">${pokemon.name}</div>
+        <div class="poke-types">${typeString}</div>
+      </div>
+    </div> <!--.card -->`
+  )
+}
+
